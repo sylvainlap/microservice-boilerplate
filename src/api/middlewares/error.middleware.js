4e32c7e0f4cec7ae81e3e7bd9d9d820e4e06b4cb
sylvainlap <sylvain.lapendry@bleexo.com>
2018-09-14 16:09:03 +0200

    remove unecessary comment

--- src/api/middlewares/error.middleware.js
+++ src/api/middlewares/error.middleware.js
@@ -1,19 +1,11 @@
 const { env } = require('../../config/vars');
 
-/**
- * Catch 404 and forward to error handler
- * @public
- */
 exports.notFound = (req, res, next) => {
   const err = new Error('Not found');
   err.statusCode = 404;
   return next(err);
 };
 
-/**
- * Error handler. Send stacktrace only during development
- * @public
- */
 exports.handler = (err, req, res, next) => {
   const statusCode = err.statusCode || 500;
 
